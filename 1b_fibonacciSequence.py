a0eb5900c64016c0fec74c2b65e1dcfcf35a8e74
ArunDK153 <54391160+ArunDK153@users.noreply.github.com>
2019-08-22 12:36:13 +0530

    Update 1b_fibonacciSequence.py

--- 1b_fibonacciSequence.py
+++ 1b_fibonacciSequence.py
@@ -4,9 +4,11 @@ def fibo(n):
     else:
         return(fibo(n-1)+fibo(n-2))
 noOfTerms = int(input("Enter the number of Fibonacci numbers to generate: "))
+fibo_list=[]
 if noOfTerms<=0:
     print("Please enter number greater than 0")
 else:
     print("Fibonacci sequence: ")  
-    for i in range(noOfTerms):  
-       print(fibo(i))  
+    for i in range(noOfTerms):
+       fibo_list.append(fibo(i)) 
+print(fibo_list) 
