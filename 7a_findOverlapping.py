7576144838431e487f3d295dadc8d517edf96ccb
ArunDK153 <54391160+ArunDK153@users.noreply.github.com>
2019-10-10 13:02:00 +0530

    Update 7a_findOverlapping.py

--- 7a_findOverlapping.py
+++ 7a_findOverlapping.py
@@ -9,5 +9,5 @@ def intersection(list1,list2):
     listSet = set(list2)
     list3 = [value for value in list1 if value in listSet]
     return list3
-print("Overlapping numbers between prime and happy numbers under 1000: \n")  
+print("Overlapping numbers between prime and happy numbers under 1000:")  
 print(*intersection(happyList,primeList), sep=", ")
